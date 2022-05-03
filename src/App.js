4727843a22b36155fbd5490bca9e5026b611cf61
Martin Stemmle <hi@martn.st>
2022-05-03 11:04:53 +0200

    Martin was here

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ export default function App() {
       <h1 className="text-2xl lg:text-5xl mb-10 text-right">
         Welcome to Your New React App{" "}
         <span className="block text-lg text-blue-400">on DigitalOcean</span>
+        Martin was here
       </h1>
 
       <div className="grid grid-cols-2 grid-rows-2 gap-4">
